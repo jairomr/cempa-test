316151b010124c8dbd8e5603a2d4a1fb00e5cf6a
Jairo Matos da Rocha <jairo@discente.ufg.br>
2022-04-28 13:51:00 -0300

    barra

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -401,43 +401,7 @@ export class AppComponent implements OnInit {
       })
     });
 
-    // this.mapService.getWinds().subscribe(res => {
-    //   // const windLayer = new WindLayer(res,{
-    //   //   windOptions: {
-    //   //     // colorScale: (m) => {
-    //   //     //   // console.log(m);
-    //   //     //   return '#fff';
-    //   //     // },
-    //   //     colorScale: [
-    //   //       "rgb(36,104, 180)",
-    //   //       "rgb(60,157, 194)",
-    //   //       "rgb(128,205,193 )",
-    //   //       "rgb(151,218,168 )",
-    //   //       "rgb(198,231,181)",
-    //   //       "rgb(238,247,217)",
-    //   //       "rgb(255,238,159)",
-    //   //       "rgb(252,217,125)",
-    //   //       "rgb(255,182,100)",
-    //   //       "rgb(252,150,75)",
-    //   //       "rgb(250,112,52)",
-    //   //       "rgb(245,64,32)",
-    //   //       "rgb(237,45,28)",
-    //   //       "rgb(220,24,32)",
-    //   //       "rgb(180,0,35)"
-    //   //     ],
-    //   //     // velocityScale: 1 / 20,
-    //   //     // paths: 5000,
-    //   //     frameRate: 16,
-    //   //     maxAge: 60,
-    //   //     globalAlpha: 0.9,
-    //   //     velocityScale: 1 / 30,
-    //   //     // paths: 10000,
-    //   //     generateParticleOption: true,
-    //   //   },
-    //   // });
-    //   //
-    //   // this.map?.addLayer(windLayer);
-    // })
+
   }
 
   creatLayer(layer: number){
